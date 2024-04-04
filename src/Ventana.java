11c52ac893702e2e0add9f499f9722c563f0b7d4
Alejandra. D <alejandra.davila.tipan@udla.edu.ec>
2024-04-04 14:09:49 -0500

    Interfaz new

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana {
     private JPanel ventana;
@@ -8,4 +10,17 @@ public class Ventana {
     private JButton cimaButton;
     private JButton buscarButton;
     private JTextArea textArea1;
+    private Pila pila = new Pila();
+
+    public Ventana(){
+
+        apilarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pila.apilar(Interger.parseInt(textNumero.getText()));
+                textArea1.setText(pila.toString());
+            }
+        });
+
+    }
 }
