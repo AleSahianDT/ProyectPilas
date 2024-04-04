4383e2b0a92e227dbd07c462f31593f5a9803747
Alejandra. D <alejandra.davila.tipan@udla.edu.ec>
2024-04-04 14:03:31 -0500

    Creado ventana (Interfaz)

---
+++ src/Pila.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+public class Pila {
+
+    private Stack<Integer> numeros;
+
+    public Pila(){
+        numeros = new Stack<Integer>();
+    }
+
+     public boolean estaVacia() {
+         return numeros.empty();
+     }
+
+     public void apilar(Integer elemento){
+        numeros.push(elemento);
+     }
+
+    public Integer desapilar() throws Exception{
+         if(estaVacia())
+             throw new Exception ("No hay elementos en la pila");
+         return numeros.pop();
+    }
+
+    public Integer cima() throws Exception{
+        if(estaVacia())
+            throw new Exception ("No hay elementos en la pila");
+        return numeros.peek();
+    }
+
+    public int buscarElemento(int i)throws Exception{
+         if(estaVacia())
+             throw new Exception ("No hay elementos en la pila");
+        return numeros.search(i);
+    }
+
+    @Override
+    public String toString() {
+        String mensaje = "";
+        for (int i = numeros.size() - 1; i >= 0; i--) {
+            mensaje += numeros.get(i) + "\n";
+        }
+        return mensaje;
+    }
+}
---
+++ src/Ventana.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Ventana {
+    private JPanel ventana;
+    private JTextField textNumero;
+    private JButton apilarButton;
+    private JButton desapilarButton;
+    private JButton cimaButton;
+    private JButton buscarButton;
+    private JTextArea textArea1;
+}
