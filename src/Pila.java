6cffd19ddda70740b236523759e4a1725f3a8521
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2024-04-10 17:41:40 -0500

    Ventana Binario

--- src/Pila.java
+++ src/Pila.java
@@ -1,43 +1,57 @@
 import java.util.Stack;
+
 public class Pila {
+    private Stack <Integer> numeros;
 
-    private Stack<Integer> numeros;
-
-    public Pila(){
+    public Pila() {
         numeros = new Stack<Integer>();
     }
 
-     public boolean estaVacia() {
-         return numeros.empty();
-     }
+    public boolean estaVaacia(){
+        return numeros.empty();
+    }
 
-     public void apilar(Integer elemento){
+    public void apilar(Integer elemento){
         numeros.push(elemento);
-     }
+    }
 
-    public Integer desapilar() throws Exception{
-         if(estaVacia())
-             throw new Exception ("No hay elementos en la pila");
-         return numeros.pop();
+    public Integer desapilar() throws Exception {
+        if (estaVaacia())
+            throw new Exception("No hay elementos");
+        return numeros.pop();
+        //POP PARA SACAR ELEMENTOS
     }
 
     public Integer cima() throws Exception{
-        if(estaVacia())
-            throw new Exception ("No hay elementos en la pila");
+        if (estaVaacia())
+            throw new Exception("No hay elementos");
         return numeros.peek();
     }
 
-    public int buscarElemento(int i)throws Exception{
-         if(estaVacia())
-             throw new Exception ("No hay elementos en la pila");
+    public int buscarElemento(int i) throws Exception{
+        if (estaVaacia())
+            throw new Exception("No hay elementos");
         return numeros.search(i);
     }
 
+    public String convertirBinario(){
+        double[] binario = {1000};
+        int exp = 0;
+        double decimal = 0;
+
+        for(int i = 0; i < binario.length; i++) {
+            for(exp = 0; exp < 4; exp++) {
+                decimal = binario[i] * (int)Math.pow(2, exp);
+            }
+        }
+        return String.valueOf(binario);
+    }
+
     @Override
-    public String toString() {
+    public String toString(){
         String mensaje = "";
-        for (int i = numeros.size() - 1; i >= 0; i--) {
-            mensaje += numeros.get(i) + "\n";
+        for (int i=numeros.size()-1; i>= 0; i--){
+            mensaje += numeros.get(i)+ "\n";
         }
         return mensaje;
     }
---
+++ src/VentanaBinario.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaBinario {
+    private JPanel ventanabinario;
+    private JTextField textNumero;
+    private JButton Convertir;
+    private JTextArea textArea1;
+    private Pila pila = new Pila();
+
+    public VentanaBinario(){
+        Convertir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"El numero decimal dado, en binario es "+textNumero.getText());
+                textArea1.setText(pila.convertirBinario());
+            }
+        });
+
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VentanaBinario");
+        frame.setContentPane(new VentanaBinario().ventanabinario);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
